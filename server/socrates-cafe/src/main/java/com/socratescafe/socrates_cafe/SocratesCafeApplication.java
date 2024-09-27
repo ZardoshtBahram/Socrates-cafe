aef428d23d96858ad3343568a607435ac32e24e5
Bahram Zardosht <zardoshtbahram81@gmail.com>
2024-09-26 20:57:19 -0400

    Setting up Client and server side repos

---
+++ server/socrates-cafe/src/main/java/com/socratescafe/socrates_cafe/SocratesCafeApplication.java
@@ -0,0 +1,13 @@
+package com.socratescafe.socrates_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocratesCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocratesCafeApplication.class, args);
+	}
+
+}
---
+++ server/socrates-cafe/src/test/java/com/socratescafe/socrates_cafe/SocratesCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socratescafe.socrates_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocratesCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
